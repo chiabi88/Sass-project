e6f25c61b534a22ba5b526d76a58490d858c472a
chiabi88 <chiabi88@gmail.com>
2017-08-09 13:50:05 +0900

    shape 믹스인 추가

--- playground/project/src/js/index.js
+++ playground/project/src/js/index.js
@@ -15,12 +15,76 @@
 	 */
 	
 	function activeGnb() {
-		var body = $('body'),
-				btn = $('#header .btn-gnb'),
-				gnb = $('#gnb'),
-				gnb_menu = $('.gnb-menu'),
-				gnb_mask = $('.gnb-wrap__mask');
+		var body, btn, gnb, gnb_menu, gnb_mask, accordionMenu;
 
+		body = $('body');
+		btn = $('#header .btn-gnb');
+		gnb = $('#gnb');
+		gnb_menu = $('.gnb-menu');
+		gnb_mask = $('.gnb-wrap__mask');
+		accordionMenu = {
+			is: '--is-depth',
+			open: '--open',
+			curr: null,
+			list: null,
+			init : function(obj) {
+				accordionMenu.curr = obj;
+				accordionMenu.list = $('[class*=' + accordionMenu.is + ']', accordionMenu.curr);
+				accordionMenu.list.children('a').on('click', function(e){
+					e.preventDefault();
+					var obj_parent = $(this).parent(); 
+					accordionMenu.menuOpen(obj_parent);
+				});
+				return false;
+			},
+			// 전체 닫기
+			reset : function(obj) {
+				accordionMenu.curr = obj;
+				accordionMenu.menuClose(accordionMenu.curr.find('li'));
+			},
+			//  기존클래스에 '*--open'을 추가한 클래스를 구한다.
+			setOpenClass : function(obj) {
+				// class 이름을 찾아 class_open에 저장함
+				var class_name, class_open;
+				accordionMenu.curr = obj; 
+				if(accordionMenu.curr.attr('class')){
+					class_name = accordionMenu.curr.attr('class').split(' ')[0]; 
+					class_name = class_name.substring(0, class_name.length - accordionMenu.is.length); 
+					class_open = class_name + accordionMenu.open;
+					return class_open;
+				} else {
+					return false;
+				}
+			},
+			// 메뉴 오픈
+			menuOpen : function(obj) {
+				accordionMenu.curr = obj;
+				var class_name = accordionMenu.setOpenClass(obj); 
+				if(accordionMenu.curr.hasClass(class_name)) { 
+					accordionMenu.menuClose(accordionMenu.curr);
+				} else {
+					accordionMenu.curr.addClass(class_name);
+					accordionMenu.menuClose(accordionMenu.curr.siblings());
+				}
+			},
+			// 메뉴 닫기
+			menuClose : function(obj) {
+				var this_children, class_name;
+				accordionMenu.curr = obj;
+				class_name = accordionMenu.setOpenClass(accordionMenu.curr);
+				accordionMenu.curr.removeClass(class_name);
+				this_children = accordionMenu.curr.find('li');
+				this_children.each(function(){
+					class_name = accordionMenu.setOpenClass($(this));
+					if($(this).hasClass(class_name)){
+						$(this).removeClass(class_name);
+					}
+				});
+			}
+		};
+
+		// 버튼 클릭시 
+		accordionMenu.init(gnb_menu);
 		btn.on('click', function(){
 			var this_obj = $(this);
 			if(gnb.hasClass('gnb-wrap--open')){
@@ -30,70 +94,13 @@
 			} else {
 				this_obj.addClass('btn-gnb--active');
 				gnb.addClass('gnb-wrap--open');
-				accordionMenu.init(gnb_menu);
 			}
 		});
+		// 배경 클릭시
 		gnb_mask.on('click', function(){
 				btn.removeClass('btn-gnb--active');
 				gnb.removeClass('gnb-wrap--open');
-			accordionMenu.reset(gnb_menu);
+				accordionMenu.reset(gnb_menu);
 		});
-
-		// accorionMenu
-		var accordionMenu = {
-			is: '--is-depth',
-			open: '--open',
-			curr: null,
-			list: null,
-			init : function(obj) {
-				accordionMenu.curr = obj;
-				accordionMenu.list = $('[class*=' + accordionMenu.is + ']', accordionMenu.curr);
-				console.log('list :', accordionMenu.list);
-				accordionMenu.list.children('a').on('click', function(e){
-					e.preventDefault();
-					var obj_parent = $(this).parent();
-					console.log('obj_parent: ',obj_parent);
-					accordionMenu.menuOpen(obj_parent);
-				});
-			},
-			reset : function(obj) {
-				accordionMenu.curr = obj;
-				accordionMenu.menuClose(accordionMenu.curr.find('li'));
-				accordionMenu.list = null;
-			},
-			setOpenClass : function(obj) {
-				// class 이름을 찾아 class_open에 저장함
-				var class_name, class_open;
-				accordionMenu.curr = obj;
-				if(accordionMenu.curr.attr('class')){
-					class_name = accordionMenu.curr.attr('class').split(' ')[0];
-					class_name = class_name.substring(0, class_name.length - accordionMenu.is.length);
-					class_open = class_name + accordionMenu.open;
-					return class_open;
-				}
-				return false;
-			},
-			menuOpen : function(obj) {
-				accordionMenu.curr = obj;
-				var class_name = accordionMenu.setOpenClass(obj);
-				console.log('class_name: ', class_name);
-				if(accordionMenu.curr.hasClass(class_name)) {
-					accordionMenu.menuClose(accordionMenu.curr);
-				} else {
-					accordionMenu.curr.addClass(class_name);
-					accordionMenu.menuClose(accordionMenu.curr.siblings());
-				}
-			},
-			menuClose : function(obj) {
-				accordionMenu.curr = obj;
-				var this_children = obj.find('li');
-				var class_name = accordionMenu.setOpenClass(obj);
-				accordionMenu.curr.removeClass(class_name);
-				this_children.each(function(){
-					class_name = accordionMenu.setOpenClass($(this));
-					$(this).removeClass(class_name);
-				});
-			}
-		};
 	};
 }(jQuery));
